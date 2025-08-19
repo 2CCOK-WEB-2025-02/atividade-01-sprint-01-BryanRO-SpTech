5bc2d0a9f6aae4fa1b33428c6d5e42178ef6bbef
BryanRO-SpTech <bryan.grocha@sptech.school>
2025-08-19 20:34:27 -0300

    Quase acabando o ultimo exercicio

--- src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
@@ -1,14 +1,20 @@
 package school.sptech.sprint1_nota1.ex2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioMedioController {
 
     @GetMapping("/ex-02/{numero}")
-    public Boolean exercicioMedio(int numero) {
+    public Boolean exercicioMedio(@PathVariable int numero) {
+        if(numero == 2) {
+            return true;
+        }
 
-        return null;
+        if(numero <= 1) return false;
+
+        return numero % 2 != 0;
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -4,11 +4,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+        List<Integer> valores = new ArrayList<>();
+
+        valores.add(0);
+        valores.add(1);
+
+        for(int i = 1; i < n; i++) {
+            int ultimoValor = valores.get(valores.size() - 1);
+            int penultimoValor = valores.get(valores.size() - 2);
+
+            valores.add(penultimoValor + ultimoValor);
+        }
+
+        ExercicioDificilResponse response = new ExercicioDificilResponse();
+
+        response.setEnesimoTermo(valores.get(valores.size() - 1));
+        response.setSoma(0);
+
+        return response;
     }
 }
