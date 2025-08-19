2a42c25233a155706dfcef0a65f7830d291d84d4
BryanRO-SpTech <bryan.grocha@sptech.school>
2025-08-19 18:45:09 -0300

    Já foi o 1 e 2

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,24 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+        String palavraLowerCase = palavra.toLowerCase();
+
+        List<String> listPalavra = new ArrayList<>(Arrays.stream(palavraLowerCase.split("")).toList());
+
+        Collections.reverse(listPalavra);
+
+        return listPalavra.equals(Arrays.stream(palavraLowerCase.split("")).toList());
+
+//        return palavra.equalsIgnoreCase(palavraInvertida);
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex2/ExercicioMedioController.java
@@ -8,6 +8,7 @@ public class ExercicioMedioController {
 
     @GetMapping("/ex-02/{numero}")
     public Boolean exercicioMedio(int numero) {
+
         return null;
     }
 }
